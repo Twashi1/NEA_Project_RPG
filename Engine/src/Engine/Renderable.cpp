df439c5ebcad56f8dd6d438ffaf4d159d3282cb7
Twashi1 <thomas.zarri@gmail.com>
2022-07-01 13:14:46 +0100

    Renderables now store a pointer to a texture (but not always used)

--- Engine/src/Engine/Renderable.cpp
+++ Engine/src/Engine/Renderable.cpp
@@ -24,14 +24,32 @@ Renderable::Renderable(std::shared_ptr<Quad> quad, Shader* shader, int z)
     m_Register(z);
 }
 
+Renderable::Renderable(std::shared_ptr<Quad> quad, Shader* shader, Texture* texture, int z)
+    : quad(quad), shader(shader), z(z), texture(texture)
+{
+    m_Register(z);
+}
+
 Renderable::Renderable(std::shared_ptr<Quad> quad, Shader* shader, int z, bool isGameWorld)
     : quad(quad), shader(shader), z(z), isGameWorld(isGameWorld)
 {
     m_Register(z);
 }
 
+Renderable::Renderable(std::shared_ptr<Quad> quad, Shader* shader, Texture* texture, int z, bool isGameWorld)
+    : quad(quad), shader(shader), z(z), texture(texture), isGameWorld(isGameWorld)
+{
+    m_Register(z);
+}
+
 Renderable::Renderable(std::shared_ptr<Quad> quad, Shader* shader, int z, bool isGameWorld, bool isVisible)
     : quad(quad), shader(shader), z(z), isGameWorld(isGameWorld), isVisible(isVisible)
+{
+    m_Register(z);
+}
+
+Renderable::Renderable(std::shared_ptr<Quad> quad, Shader* shader, Texture* texture, int z, bool isGameWorld, bool isVisible)
+    : quad(quad), shader(shader), z(z), texture(texture), isGameWorld(isGameWorld), isVisible(isVisible)
 {
     m_Register(z);
 }
--- Engine/src/Engine/Renderable.h
+++ Engine/src/Engine/Renderable.h
@@ -4,6 +4,7 @@
 #include "Renderer.h"
 #include "Shader.h"
 #include "Quad.h"
+#include "Texture.h"
 
 struct ENGINE_API Renderable {
 private:
@@ -15,12 +16,17 @@ private:
 public:
     std::shared_ptr<Quad> quad; // Describes the location and dimensions of object to render
     Shader* shader;             // Pointer to shader
+    Texture* texture = nullptr; // Pointer to texture if it has one
     bool isVisible = true;      // Determines if renerable will be drawn
     bool isGameWorld = true;    // If renderable exists in game world (if its not in the game world, it won't be transformed by the camera)
 
+    // Think of better way to pass parameters
     Renderable(std::shared_ptr<Quad> quad, Shader* shader, int z);
+    Renderable(std::shared_ptr<Quad> quad, Shader* shader, Texture* texture, int z);
     Renderable(std::shared_ptr<Quad> quad, Shader* shader, int z, bool isGameWorld);
+    Renderable(std::shared_ptr<Quad> quad, Shader* shader, Texture* texture, int z, bool isGameWorld);
     Renderable(std::shared_ptr<Quad> quad, Shader* shader, int z, bool isGameWorld, bool isVisible);
+    Renderable(std::shared_ptr<Quad> quad, Shader* shader, Texture* texture, int z, bool isGameWorld, bool isVisible);
 
     // Set new z value
     void SetZ(int nz);
