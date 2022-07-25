f98d670f576694fb6ba5946e781648b9f610e92a
Twashi1 <thomas.zarri@gmail.com>
2022-07-25 16:23:22 +0100

    fixed bug

--- Engine/src/Engine/Panel.h
+++ Engine/src/Engine/Panel.h
@@ -1,40 +1,31 @@
 #pragma once
 
 #include "Utils.h"
-#include "Engine.h"
 #include "Quad.h"
-#include "Shader.h"
-#include "Texture.h"
-
-// TODO:
-// Panels should be resizeable
-// Panels should be moveable
-// Panels should be resized with window resize (or at least clamped to window max/min)
-// so much logic and code needed for this damn
-// end api should be something like
 
 /*
 Panel p({200, 200, 300, 300});
 Button my_button(some_params); // the x and y given will be the offset from the anchor
 
-p.Anchor(my_button);
+Engine::window_panel->Anchor(ANCHOR::RIGHT, ANCHOR::TOP, &p);
+
+p.Anchor(my_button); // This will edit multiple parameters in button class, since both text and quad has to be moved
 or
-p.Anchor(<&>my_button.quad); // Should this return the pointer?
+p.Anchor(quad_ptr);
 */
-class Panel {
+class ENGINE_API Panel {
 public:
-	enum ANCHOR : uint8_t {
+	enum class ENGINE_API ANCHOR : uint8_t {
 		LEFT, RIGHT,
 		BOTTOM, TOP,
 		CENTER
 	};
 
 private:
-	static std::vector<Panel*> m_Panels; // Store list of all panels, so they can be edited when window is resized
-
-	struct Data {
+	struct ENGINE_API Data {
 	private:
-		enum class Type : uint8_t {
+		enum class ENGINE_API Type : uint8_t {
+			PANEL,
 			QUAD,
 			POINT
 		};
@@ -43,47 +34,54 @@ private:
 
 	public:
 		union {
+			ENG_Ptr(Panel) panel;
 			ENG_Ptr(Quad) quad;
 			ENG_Ptr(Vector2<float>) point;
 		};
-		
-		ANCHOR x_anchor, y_anchor;
 
-		Data(ANCHOR x, ANCHOR y, ENG_Ptr(Quad) quad);
-		Data(ANCHOR x, ANCHOR y, ENG_Ptr(Vector2<float>) point);
+		Vector2<float> last_pos;
+		Vector2<float> offset;
+
+		void SetPos(const Vector2<float>& pos);
+		Vector2<float> GetPos() const;
+
+		// Assuming object passed has position relative to anchor coordinates
+		Data(ANCHOR x, ANCHOR y, ENG_Ptr(Quad) quad, const Vector2<float>& offset = Vector2<float>(FLT_MAX, FLT_MAX));
+		Data(ANCHOR x, ANCHOR y, ENG_Ptr(Vector2<float>) point, const Vector2<float>& offset = Vector2<float>(FLT_MAX, FLT_MAX));
+		Data(ANCHOR x, ANCHOR y, ENG_Ptr(Panel) panel, const Vector2<float>& offset = Vector2<float>(FLT_MAX, FLT_MAX));
+		Data(const Data& other);
+		~Data();
+
+		Data& operator=(const Data& other);
+
+		ANCHOR x_anchor, y_anchor;
 
 		friend Panel;
 	};
 
 	// All quads/points we're editing
-	std::vector<Data> m_PanelObjects;
+	std::vector<Data*> m_PanelObjects;
 
+	Vector2<float> old_dim;
+	Vector2<float> old_pos;
 	ENG_Ptr(Quad) m_Quad;
 
-	void m_UpdatePos(Data& panel_object, const Vector2<float>& old_pos, const Vector2<float>& new_pos);
-	void m_UpdateDim(Data& panel_object, const Vector2<float>& old_dim, const Vector2<float>& new_dim);
-	void m_UpdateQuad(Data& panel_object, const Quad& old_quad, const Quad& new_quad);
+	void m_Update(Data& panel_object, const Rect& new_rect);
 
 public:
 	// Set panel coordinates/size
 	Panel(ENG_Ptr(Quad) quad);
+	Panel(const Quad& quad);
+	Panel(Quad* quad);
 	~Panel();
-
-	// TODO only called by engine, could be private
-	static void UpdateDim(const Vector2<float>& new_dim);
 
 	void SetPos(const Vector2<float>& new_pos);
 	void SetDim(const Vector2<float>& new_dim);
-	void SetQuad(const Quad& new_quad);
-
-	void Anchor(ANCHOR x, ANCHOR y, Quad& quad);
-	void Anchor(ANCHOR x, ANCHOR y, Vector2<float>& point);
-
-	void Anchor(ANCHOR x, ANCHOR y, Quad* quad);
-	void Anchor(ANCHOR x, ANCHOR y, Vector2<float>* point);
+	void SetRect(const Rect& new_rect);
 
 	void Anchor(ANCHOR x, ANCHOR y, ENG_Ptr(Quad) quad);
 	void Anchor(ANCHOR x, ANCHOR y, ENG_Ptr(Vector2<float>) point);
+	void Anchor(ANCHOR x, ANCHOR y, ENG_Ptr(Panel) panel);
 
 	// For moving/resizing panels (although this requires the panel to be drawn through something)
 	void Update();
