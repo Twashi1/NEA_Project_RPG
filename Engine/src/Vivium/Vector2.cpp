db5985bca4202966bbcfdd053ed28ebcdebc7c24
Twashi1 <thomas.zarri@gmail.com>
2022-11-21 21:15:31 +0000

    useless spec

--- Engine/src/Vivium/Vector2.cpp
+++ Engine/src/Vivium/Vector2.cpp
@@ -7,7 +7,6 @@ namespace Vivium {
 	template struct VIVIUM_API Vector2<uint16_t>;
 	template struct VIVIUM_API Vector2<int32_t>;
 	template struct VIVIUM_API Vector2<uint32_t>;
-	template struct VIVIUM_API Vector2<std::size_t>;
 	template struct VIVIUM_API Vector2<long>;
 	template struct VIVIUM_API Vector2<float>;
 	template struct VIVIUM_API Vector2<double>;
